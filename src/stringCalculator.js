7c3ad5c26f062e4b353cf29dfe27533893c3c34d
virajsathvara <virajsathvara@gmail.com>
2022-04-08 11:42:35 +0530

    feat: calculator can handle multiple numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,12 @@
 module.exports = function stringCalculator (numbers) {
   if (numbers === "") return 0
   const argArray = numbers.split(",")
+  console.log('array is: ', argArray);
   if (argArray.length === 1) return parseInt(argArray[0])
+
+  let total = 0
+  for (let i of argArray) {
+    total += parseInt(i)
+  }
+  return total
 }
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -7,4 +7,8 @@ describe("string calculator tests", () => {
   test('should return the argument when passed only one', () => {
     expect(calculator("1")).toBe(1);
   });
+
+  test('should be able to handle multiple inputs', () => {
+    expect(calculator("1,2")).toBe(3);
+  });
 })
