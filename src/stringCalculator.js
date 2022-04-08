428baceb5daf85904e7a8f961ff351e977015001
virajsathvara <virajsathvara@gmail.com>
2022-04-08 11:35:44 +0530

    feat: handling one argument

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,5 @@
-module.exports = function stringCalculator (number) {
-  if (number === "") return 0
+module.exports = function stringCalculator (numbers) {
+  if (numbers === "") return 0
+  const argArray = numbers.split(",")
+  if (argArray.length === 1) return parseInt(argArray[0])
 }
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,4 +3,8 @@ describe("string calculator tests", () => {
   test('should accept empty string', () => {
     expect(calculator("")).toBe(0);
   });
+
+  test('should return the argument when passed only one', () => {
+    expect(calculator("1")).toBe(1);
+  });
 })
